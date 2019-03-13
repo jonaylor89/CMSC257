dfe5d96da92b3d7ca25c77a364763284e346429e
John Naylor <jonaylor89@gmail.com>
2019-03-13 13:16:48 -0400

    Comment the actual assignment

--- asgn3/asgn3.c
+++ asgn3/asgn3.c
@@ -3,7 +3,13 @@
 
 int main(int argc, char **argv) {
 
+  /* Make 10 calls to malloc */
 
+  /* Make 10 calls to calloc */
+
+  /* Make 10 calls to realloc */
+
+  /* Make 5 calls to free */
 
   return 0;
 }
