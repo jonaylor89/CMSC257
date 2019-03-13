2ed655da5e9010cab7779862dd96b2b56170c9db
John Naylor <jonaylor89@gmail.com>
2019-03-13 11:08:56 -0400

    Includes

--- asgn3/asgn3_support.h
+++ asgn3/asgn3_support.h
@@ -2,6 +2,10 @@
 #ifndef ASGN3_SUPPORT
 #define ASGN3_SUPPORT
 
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 void *malloc(size_t size);
 void *calloc(size_t nitems, size_t size);
 void *realloc(void *ptr, size_t size);
