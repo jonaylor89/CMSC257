0bef924ec31c86dd0e37ec1d0ee73d1c3760d111
John Naylor <jonaylor89@gmail.com>
2019-03-15 14:10:08 -0400

    calloc and realloc

--- asgn3/asgn3.c
+++ asgn3/asgn3.c
@@ -9,6 +9,14 @@ int main(int argc, char **argv) {
    int c;
    int[] d;
    int[] e;
+   int[] f;
+   int[] g;
+   int[] h;
+   int[] i;
+   int[] j;
+   int[] k;
+   int[] l;
+   int[] m;
 
   if ((a = malloc(sizeof(int))) == NULL) {
     return 1; 
@@ -31,10 +39,129 @@ int main(int argc, char **argv) {
     return 1; 
   }
 
+  /* Do it again because why not */
+  if ((a = malloc(sizeof(int))) == NULL) {
+    return 1; 
+  }
+
+  if ((b = malloc(sizeof(int))) == NULL) {
+    return 1; 
+  }
+
+  if ((c = malloc(sizeof(int))) == NULL) {
+    return 1; 
+  }
+
+  if ((d = malloc(sizeof(int) * 4)) == NULL) {
+    return 1; 
+  }
+
+
+  if ((e = malloc(sizeof(int) * 4)) == NULL) {
+    return 1; 
+  }
+ 
+
   /* Make 10 calls to calloc */
 
+  if ((d = calloc(sizeof(int), 4)) == NULL) {
+    return 1; 
+  }
+
+
+  if ((e = calloc(sizeof(int), 4)) == NULL) {
+    return 1; 
+  }
+ 
+
+  if ((f = calloc(sizeof(int), 4)) == NULL) {
+    return 1; 
+  }
+
+
+  if ((g = calloc(sizeof(int), 4)) == NULL) {
+    return 1; 
+  }
+ 
+
+  if ((h = calloc(sizeof(int), 4)) == NULL) {
+    return 1; 
+  }
+
+
+  if ((i = calloc(sizeof(int), 4)) == NULL) {
+    return 1; 
+  }
+ 
+
+  if ((j = calloc(sizeof(int), 4)) == NULL) {
+    return 1; 
+  }
+
+
+  if ((k = calloc(sizeof(int), 4)) == NULL) {
+    return 1; 
+  }
+ 
+  if ((l = calloc(sizeof(int), 4)) == NULL) {
+    return 1; 
+  }
+
+
+  if ((m = calloc(sizeof(int), 4)) == NULL) {
+    return 1; 
+  }
+ 
 
   /* Make 10 calls to realloc */
+
+  if ((d = realloc(d, sizeof(int) * 10)) == NULL) {
+    return 1; 
+  }
+
+
+  if ((e = realloc(e, sizeof(int) + 10)) == NULL) {
+    return 1; 
+  }
+ 
+
+  if ((f = realloc(f, sizeof(int) * 10)) == NULL) {
+    return 1; 
+  }
+
+
+  if ((g = realloc(g, sizeof(int) * 10)) == NULL) {
+    return 1; 
+  }
+ 
+
+  if ((h = realloc(h, sizeof(int) * 10)) == NULL) {
+    return 1; 
+  }
+
+
+  if ((i = realloc(i, sizeof(int) * 10)) == NULL) {
+    return 1; 
+  }
+ 
+
+  if ((j = realloc(j, sizeof(int) * 10)) == NULL) {
+    return 1; 
+  }
+
+
+  if ((k = realloc(k, sizeof(int) * 10)) == NULL) {
+    return 1; 
+  }
+ 
+  if ((l = realloc(l, sizeof(int) * 10)) == NULL) {
+    return 1; 
+  }
+
+
+  if ((m = realloc(m, sizeof(int) * 10)) == NULL) {
+    return 1; 
+  }
 
   /* Make 5 calls to free */
 
