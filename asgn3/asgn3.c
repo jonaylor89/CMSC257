69aba4f4fefc2d0c096b2b826e9a91a8f6e61d94
John Naylor <jonaylor89@gmail.com>
2019-03-16 14:20:30 -0400

    proper array initialization

--- asgn3/asgn3.c
+++ asgn3/asgn3.c
@@ -1,22 +1,22 @@
 
 #include "asgn3_support.h"
 
-int main(int argc, char **argv) {
+int main(void) {
 
   /* Make 10 calls to malloc */
    int a;
    int b;
    int c;
-   int[] d;
-   int[] e;
-   int[] f;
-   int[] g;
-   int[] h;
-   int[] i;
-   int[] j;
-   int[] k;
-   int[] l;
-   int[] m;
+   int d[];
+   int e[];
+   int f[];
+   int g[];
+   int h[];
+   int i[];
+   int j[];
+   int k[];
+   int l[];
+   int m[];
 
   if ((a = malloc(sizeof(int))) == NULL) {
     return 1; 
