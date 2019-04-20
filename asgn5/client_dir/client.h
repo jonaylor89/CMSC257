efab8c7cb6a88c618193d92deaeeed1aabf345e0
John Naylor <jonaylor89@gmail.com>
2019-04-20 19:12:29 -0400

    Boilerplate stuff

---
+++ asgn5/client_dir/client.h
@@ -0,0 +1,7 @@
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
---
+++ asgn5/server_dir/server.h
@@ -0,0 +1,6 @@
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
